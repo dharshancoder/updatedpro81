4f4c445c4eb16ab47f0e0ba7c0b71b02dd677c13
dharshancoder <92212498+dharshancoder@users.noreply.github.com>
2021-10-20 15:47:49 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+canvas =
+document.getElementById("myCanvas");
+ctx = canvass.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle = red;
+ctx.lineWidth = 1;
+ctx.rect(150,143,130,200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = blue;
+ctx.lineWidth = 1;
+ctx.rect(150,143,130,200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = green;
+ctx.lineWidth = 1;
+ctx.rect(150,143,130,200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = yellow;
+ctx.lineWidth = 1;
+ctx.rect(150,143,130,200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = black;
+ctx.lineWidth = 1;
+ctx.rect(150,143,130,200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = pink;
+ctx.lineWidth = 1;
+ctx.rect(150,143,130,200);
+ctx.stroke();
